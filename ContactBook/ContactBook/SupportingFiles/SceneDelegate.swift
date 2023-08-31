ef9913f516daf0019041dc2b0be1f330870a3085
Pavel <Klopat>
2023-08-31 17:09:57 +0300

    Renamed main VC and reorganized them

--- ContactBook/ContactBook/SupportingFiles/SceneDelegate.swift
+++ ContactBook/ContactBook/SupportingFiles/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let rootVC = ViewController()
+        let rootVC = ContactsListViewController()
         window.rootViewController = UINavigationController(rootViewController: rootVC)
         window.makeKeyAndVisible()
         self.window = window
