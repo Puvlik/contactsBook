128c9e92acd4fbbb23fd2f514094c04e7856c331
Pavel <Klopat>
2023-08-31 15:58:24 +0300

    Create and setup project
    Created new project, added Swiftlint and fixed all required issues

---
+++ ContactBook/ContactBook/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ContactBook
+//
+//  Created by Паша Клопот on 31.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
